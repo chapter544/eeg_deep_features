62dc0b32ceebf0c1a13e073425cc8c28a1980f9b
Chuong nguyen <chuongvn@gmail.com>
2017-07-15 15:23:01 +0700

    Resolve conflict

--- src/tf_utils.py
+++ src/tf_utils.py
@@ -2,12 +2,8 @@ import tensorflow as tf
 import numpy as np
 
 def weight_variable(shape):
-<<<<<<< HEAD
-    initial = tf.truncated_normal(shape, stddev=0.5)
-=======
     #initial = tf.truncated_normal(shape, stddev=0.01)
     initial = tf.truncated_normal(shape, stddev=0.03)
->>>>>>> a3ed0a30cbe6b19704b4556dc6cf862fcf9e321f
     return tf.Variable(initial)
 
 def bias_variable(shape): 
